6985368e4c2d271ae787d544da3fdc0b95ba3db0
Chris Telford <christelford@hotmail.co.uk>
2022-10-30 18:43:42 -0700

    Refactored for improved file structure
     - utilities, including logging, configurations, and middleware, handled under seperate modules
     - routing under controllers folder
     - data models under  models folder

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const config = require('./utils/config');
+
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB');
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message);
+  });
+
+app.use(cors());
+// app.use(express.static('build'))
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
